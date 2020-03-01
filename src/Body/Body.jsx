72ebf5347ad4e97a532c45ceffea5a69922b7c24
Facundo Rodriguez <facurodri91@gmail.com>
2020-02-29 23:11:08 -0500

    base for app

---
+++ src/Body/Body.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const Body = () => {
+    return (
+    <div>
+        <Jumbotron fluid className= "main">
+        <h2>Welcome To the page</h2>
+        </Jumbotron>
+        </div>
+    );
+}
+export default Body;
---
+++ src/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './Header.css';
+
+const Header = () => {
+    return (
+    <div className="header">
+        <h2> Empanadas de la Yaya</h2>
+        <Button> Empanadas </Button>
+    </div>
+    );
+}
+export default Header;
